60ef920504c2a57db5b229d35a946757a8d4c435
Kannan <haikanna@gmail.com>
2018-06-02 13:48:58 +0800

    Added New Project

---
+++ VariablesSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace VariablesSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "TouchWorld";
+            var age = 1;
+            var isRabbit = true;
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type IsRabbitType = isRabbit.GetType();
+
+            WriteLine($"name is type {nameType }");
+            WriteLine($"Age is type {ageType}");
+            WriteLine($"IsRabbitType is type {IsRabbitType}");
+            ReadKey();
+        }
+    }
+}
+
